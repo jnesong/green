3038d56340abb206f45f27574c11bd76b61b1936
Jenny Chau <jnechau@gmail.com>
2022-03-29 16:01:04 -0400

    trying another graph structure

--- graphs/RouteBetweenNodes.js
+++ graphs/RouteBetweenNodes.js
@@ -1,13 +1,13 @@
 
 let graph = [
-    {node: "Harry", id: 1, neighbor1: 2, neighbor2: 3, neighbor3: 5, visited: 0},
-    {node: "Hermione", id:2, neighbor1: 6, neighbor2: 3, visited: 0},
-    {node: "Ron", id:3, neighbor1: 2, visited: 0},
-    {node: "Dumbledore", id:4, neighbor1: 1, visited: 0},
-    {node: "Hagrid", id:5, neighbor1: 4, visited: 0},
-    {node: "Ginny", id:6, neighbor1: 8, neighbor2: 3, visited: 0},
-    {node: "Neville", id:7, neighbor1: 6, neighbor2: 8, visited: 0},
-    {node: "Luna", id:8, visited: 0}
+    {node: "Harry", id: 1, nextTos: [2, 3, 5], visited: 0},
+    {node: "Hermione", id:2, nextTos: [6, 3], visited: 0},
+    {node: "Ron", id:3, nextTos: [2], visited: 0},
+    {node: "Dumbledore", id:4, nextTos:[1], visited: 0},
+    {node: "Hagrid", id:5, nextTos: [4], visited: 0},
+    {node: "Ginny", id:6, nextTos: [8,3], visited: 0},
+    {node: "Neville", id:7, nextTos: [6, 8], visited: 0},
+    {node: "Luna", id:8, nextTos: [], visited: 0}
 ]
 
 //Given a directed graph, design an algorithm to find out whether this is a route between two nodes
@@ -16,4 +16,6 @@ let graph = [
 function RouteBetweenNodes(nodeA, nodeB) {
 
 }
+
+console.log(RouteBetweenNodes(graph[0], graph[7]))
 
