a41a617ee39b661a65aa4b93dfdf711dc656096d
Jenny Chau <jnechau@gmail.com>
2022-04-05 16:54:12 -0400

    mintree in proce

--- graphs/MinimalTree.js
+++ graphs/MinimalTree.js
@@ -7,29 +7,47 @@
 
 let tree = {}
 
+
 function minimalTree(arr) {
-    let middle = Math.ceil(arr.length / 2)
-    if (middle<3){
-        tree[arr[middle]] = {v: arr[1], l:arr[0], r:arr[2]}
-        return tree[arr[middle]].v
-    }
-    let left = []
-    for (let i = 0; i < middle; i++) {
-        left.push(arr[i])
-    }
-    let right = []
-    for (let j = middle+1; j < arr.length; j++) {
-        right.push(arr[j])
+
+    let middle = getMiddle(arr)
+    console.log(arr[middle])
+    let left = [...arr.slice(0, middle)]
+    let right =[...arr.slice(middle+1, arr.length)]
+    console.log(left)
+    console.log(right)
+    tree[arr[middle]] = {
+        v: arr[middle],
+        l: left[getMiddle(left)],
+        r: right[getMiddle(right)]
     }
 
-    tree[arr[middle]] = {v: arr[middle], l: minimalTree(left), r: minimalTree(right) }
+    makeTree(left);
+    makeTree(right);
 
-    return tree[arr[middle]].v
+    console.log(tree)
 
 };
 
+function makeTree ( arr ) {
+    let middle = getMiddle(arr)
+    console.log(arr[middle])
+    let left = [...arr.slice(0, middle)]
+    let right =[...arr.slice(middle+1, arr.length)]
+    console.log(left)
+    console.log(right)
+    tree[arr[middle]] = {
+        v: arr[middle],
+        l: left[getMiddle(left)],
+        r: right[getMiddle(right)]
+    }
+}
+
+function getMiddle(arr){
+    return Math.floor(arr.length/2)
+}
+
 
 // console.log(minimalTree([-10, -3, 0, 4, 22, 109, 201]))
-console.log(minimalTree([-30, -10, -3, 0, 4, 22, 56, 109, 201]))
+console.log(minimalTree([-30, -10, -3, 0, 4, 22, 56, 109]))
 // console.log(minimalTree([1, 3, 4, 6, 7, 8, 10, 13, 14]))
-console.log(tree)
