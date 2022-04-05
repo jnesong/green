75a605e4f75bb24ef7fd050aad0fadcef5057fdf
Jenny Chau <jnechau@gmail.com>
2022-04-05 15:22:44 -0400

    minimal tree in process

--- graphs/MinimalTree.js
+++ graphs/MinimalTree.js
@@ -9,6 +9,10 @@ let tree = {}
 
 function minimalTree(arr) {
     let middle = Math.floor(arr.length / 2)
+    if (middle<2){
+        tree[arr[middle]] = {v: arr[middle], l:arr[0], r:arr[2]}
+        return tree[arr[middle]].v
+    }
     let left = []
     for (let i = 0; i < middle; i++) {
         left.push(arr[i])
@@ -18,16 +22,14 @@ function minimalTree(arr) {
         right.push(arr[j])
     }
 
-    tree[arr[middle]] = { a: arr[middle-1], b: arr[middle+1] }
+    tree[arr[middle]] = {v: arr[middle], l: minimalTree(left), r: minimalTree(right) }
 
-    console.log(left)
-    console.log(right)
+    return tree[arr[middle]].v
 
-    if (middle > 1) {
-        minimalTree(left)
-        minimalTree(right)
-    }
-}
+};
 
-console.log(minimalTree([-10, -3, 0, 4, 22, 109, 201]))
+
+
+// console.log(minimalTree([-10, -3, 0, 4, 22, 109, 201]))
+console.log(minimalTree([-30, -10, -3, 0, 4, 22, 56, 109, 201]))
 console.log(tree)
